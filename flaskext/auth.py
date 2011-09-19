1d51306236d9f26a3ab52f324b3ee319ab714c02
Charles Leifer <coleifer@gmail.com>
2011-09-19 12:39:00 -0500

    Modify password rehashing improved

--- flaskext/auth.py
+++ flaskext/auth.py
@@ -63,10 +63,16 @@ class Auth(object):
             columns = ['username', 'email', 'active', 'admin']
             
             def save_model(self, instance, form, adding=False):
+                orig_password = ''
+                if not adding:
+                    orig_password = instance.password
+                
                 user = super(UserAdmin, self).save_model(instance, form, adding)
-                if adding:
+                
+                if orig_password != form.password.data:
                     user.set_password(form.password.data)
                     user.save()
+                
                 return user
                 
         
