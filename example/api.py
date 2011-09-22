92ff13c741a038bf416bee9fd2d561e29f08e373
Charles Leifer <coleifer@gmail.com>
2011-09-22 16:12:39 -0500

    Cleaning up the example app's API

--- example/api.py
+++ example/api.py
@@ -1,4 +1,4 @@
-from flaskext.rest import RestAPI, RestResource, UserAuthentication
+from flaskext.rest import RestAPI, RestResource, UserAuthentication, AdminAuthentication, RestrictOwnerResource
 
 from app import app
 from auth import auth
@@ -6,12 +6,25 @@ from models import User, Message, Relationship
 
 
 user_auth = UserAuthentication(auth)
+admin_auth = AdminAuthentication(auth)
+
+# instantiate our api wrapper
 api = RestAPI(app, default_auth=user_auth)
 
+
 class UserResource(RestResource):
-    exclude = ('password',)
+    exclude = ('password', 'email',)
 
 
-api.register(Message)
-api.register(Relationship)
-api.register(User, UserResource)
+class MessageResource(RestrictOwnerResource):
+    owner_field = 'user'
+
+
+class RelationshipResource(RestrictOwnerResource):
+    owner_field = 'from_user'
+
+
+# register our models so they are exposed via /api/<model>/
+api.register(User, UserResource, auth=admin_auth)
+api.register(Relationship, RelationshipResource)
+api.register(Message, MessageResource)
