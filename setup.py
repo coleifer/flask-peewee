065cb3e7ca04ef326190463bbcd6c6bff1bdcb94
Charles Leifer <coleifer@gmail.com>
2011-09-23 14:33:01 -0500

    Bumping to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flask-peewee',
-    version='0.1.1',
+    version='0.1.2',
     url='http://github.com/coleifer/flask-peewee/',
     license='BSD',
     author='Charles Leifer',
