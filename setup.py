4f44ec5583abba5099880a2a2af76404223a594b
Charles Leifer <coleifer@gmail.com>
2011-11-09 08:36:02 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flask-peewee',
-    version='0.3.1',
+    version='0.3.2',
     url='http://github.com/coleifer/flask-peewee/',
     license='BSD',
     author='Charles Leifer',
