ee716b21948ffc167169684ff3de8fae6db80477
Charles Leifer <coleifer@gmail.com>
2012-03-28 21:25:37 -0500

    Check same thread = false, fixes #25

--- example/config.py
+++ example/config.py
@@ -4,6 +4,7 @@ class Configuration(object):
     DATABASE = {
         'name': 'example.db',
         'engine': 'peewee.SqliteDatabase',
+        'check_same_thread': False,
     }
     DEBUG = True
     SECRET_KEY = 'shhhh'
