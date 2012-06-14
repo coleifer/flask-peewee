d4fe6de476fbd79fe937a355c0302101db232bb7
Charles Leifer <coleifer@gmail.com>
2012-06-14 14:13:42 -0500

    Version bump for sprites

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[:2] < (2, 6):
 
 setup(
     name='flask-peewee',
-    version='0.5.3',
+    version='0.5.4',
     url='http://github.com/coleifer/flask-peewee/',
     license='BSD',
     author='Charles Leifer',
