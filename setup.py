52d0c192b3a8cfaabdd7d7a081c5296103f6f765
Charles Leifer <coleifer@gmail.com>
2015-11-21 20:40:21 -0600

    0.6.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[:2] < (2, 6):
 
 setup(
     name='flask-peewee',
-    version='0.6.6',
+    version='0.6.7',
     url='http://github.com/coleifer/flask-peewee/',
     license='BSD',
     author='Charles Leifer',
