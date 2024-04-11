e4c8bd947b6524bb2fd93c3598e48818acf00307
Charles Leifer <coleifer@gmail.com>
2024-04-11 07:57:14 -0500

    3.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[:2] < (2, 6):
 
 setup(
     name='flask-peewee',
-    version='3.0.5',
+    version='3.0.6',
     url='http://github.com/coleifer/flask-peewee/',
     license='MIT',
     author='Charles Leifer',
